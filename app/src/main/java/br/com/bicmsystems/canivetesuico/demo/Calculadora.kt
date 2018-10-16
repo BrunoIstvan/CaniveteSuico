fe4cab03e705222ba2ab1ecc2a5fddb73f92de3c
Usuário Convidado <Guest@PA06MICRO14.local>
2018-10-16 19:52:10 -0300

    Initial commit

---
+++ app/src/main/java/br/com/bicmsystems/canivetesuico/demo/Calculadora.kt
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.canivetesuico.demo
+
+class Calculadora {
+
+    fun somar(v1: Int, v2: Int) : Int = v1 + v2
+
+    fun substrair(v1: Int, v2: Int) : Int = v1 - v2
+
+    fun multiplicar(v1: Int, v2: Int) : Int = v1 * v2
+
+    fun dividir(v1: Int, v2: Int) : Int = v1 / v2
+
+}
