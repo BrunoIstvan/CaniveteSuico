01c6c2b21ef922d39f5214f7ba985ec3f294bf94
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2018-10-16 20:47:56 -0300

    Troca de substrair para subtrair

--- app/src/main/java/br/com/bicmsystems/canivetesuico/demo/Calculadora.kt
+++ app/src/main/java/br/com/bicmsystems/canivetesuico/demo/Calculadora.kt
@@ -4,7 +4,7 @@ class Calculadora {
 
     fun somar(v1: Int, v2: Int) : Int = v1 + v2
 
-    fun substrair(v1: Int, v2: Int) : Int = v1 - v2
+    fun subtrair(v1: Int, v2: Int) : Int = v1 - v2
 
     fun multiplicar(v1: Int, v2: Int) : Int = v1 * v2
 
